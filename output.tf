441541c0218beac69defad731f11d8761c998f5a
Dayron Remigio Hernandez (GLOBANT) <dayronremigio.globant@latam.com>
2023-03-17 10:15:02 -0300

    add new logic in product.tf

--- output.tf
+++ output.tf
@@ -1,18 +1,10 @@
-
-output "object_id" {
-  description = "The application's object ID."
-  value       = azuread_application.main.object_id
-}
-output "tenant_id" {
-  value = data.azuread_client_config.current.tenant_id
+output "region" {
+  value       = var.full.*.region
+  description = "GCloud Region"
 }
 
-
-output "client_id" {
-  value = data.azuread_client_config.current.client_id
+output "project_id" {
+  value       = var.full.*.project_id
+  description = "GCloud Project ID"
 }
 
-output "single_spa_redirect_uris" {
-  description = "The application's redirect redirect_uris."
-  value = azuread_application.main.single_page_application[0].redirect_uris != null ? azuread_application.main.single_page_application[0].redirect_uris : null
-}
--- provider.tf
+++ provider.tf
@@ -1,17 +1,8 @@
 terraform {
   required_providers {
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = ">= 2.33.0"
-    }
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.42.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.4.3"
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
     }
   }
-  required_version = ">= 0.13"
 }
