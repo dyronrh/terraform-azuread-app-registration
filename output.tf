5d1f0b924f357653eb534e7b837e843366b03e67
Dayron Remigio Hernandez (GLOBANT) <dayronremigio.globant@latam.com>
2023-03-22 22:38:10 -0300

    [FIX] bad output variable

--- output.tf
+++ output.tf
@@ -16,17 +16,18 @@ output "client_id" {
 
 output "azure_redirect_uri_spa" {
   description = "The application's redirect redirect_uris for spa."
-  value = length(azuread_application.main.single_page_application.redirect_uris) > 0 ? azuread_application.main.single_page_application.*.redirect_uris : null
+  value = length(azuread_application.main.single_page_application.*.redirect_uris) > 0 ? azuread_application.main.single_page_application.*.redirect_uris : null
 }
 
 
 output "azure_redirect_uri_web" {
   description = "The application's redirect redirect_uris for web."
-  value = length(azuread_application.main.web.redirect_uris) > 0 ? azuread_application.main.web.*.redirect_uris : null
+  value = length(azuread_application.main.web.*.redirect_uris) > 0 ? azuread_application.main.web.*.redirect_uris : null
 }
 
 
 output "azure_redirect_uri_public_client" {
   description = "The application's redirect redirect_uris for public_client."
-  value = length(azuread_application.main.public_client.redirect_uris)  > 0 ? azuread_application.public_client.*.redirect_uris : null
+  value = length(azuread_application.main.public_client.*.redirect_uris)  > 0 ? azuread_application.main.public_client.*.redirect_uris : null
 }
+
