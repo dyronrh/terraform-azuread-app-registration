98f7539b26c44158d7fbbd51f237826c21ea5bf5
Dayron Remigio Hernandez (GLOBANT) <dayronremigio.globant@latam.com>
2023-04-06 23:08:43 -0400

    add group

--- main.tf
+++ main.tf
@@ -186,8 +186,8 @@ resource "azuread_application" "main" {
 
 resource "azuread_app_role_assignment" "example" {
   #for_each = output.azure_roles_group
-  #for_each            = {for i,v in local.groups_r: i=>v}
-  for_each = toset(local.groups_r[0])
+  for_each            = {for i,v in local.groups_r[0]: i=>v}
+  #for_each = toset(local.groups_r[0])
 
     app_role_id         = each.value.role_id != null ?  each.value.role_id : "432e4502-173f-4152-8176-zzzzzzzzzzzzs"
     principal_object_id =  each.value.group_id
